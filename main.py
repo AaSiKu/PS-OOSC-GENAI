14e13ef0d10af6d3f56bf00f66ee64c4586b7fea
BlakkTyger <137704688+BlakkTyger@users.noreply.github.com>
2024-08-26 11:18:47 +0530

    no more dicks

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ keys = get_summ.extract_keywords(article_text)
 topics_summary_tuple_list = get_summ.text_rank(article_text, keys)
 ques_lis = get_ques.getQuestions(topics_summary_tuple_list)
 temp = [i[1] for i in topics_summary_tuple_list]
-main_summary_list = [' '.join(j) for j in dick2]
+main_summary_list = [' '.join(j) for j in temp]
 FinalQuesLis = get_best_ques.get_top_10(ques_lis,main_summary_list)
